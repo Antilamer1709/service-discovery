0a1e61d4d1c831ec4d4794b29a28117b7da480c2
vslobo <antilamer1709@gmail.com>
2020-05-21 17:04:01 +0300

    Send files

--- client2/src/main/java/com/antilamer/client/controller/ClientController.java
+++ client2/src/main/java/com/antilamer/client/controller/ClientController.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -42,11 +45,13 @@ public class ClientController {
 
 
     @PostMapping("files/upload")
-    public void upload(@RequestParam("files") MultipartFile[] files) throws IOException {
-        HttpEntity<MultiValueMap<String, Object>> requestEntity = createRequestEntity(files);
+    public String upload(@RequestParam("files") MultipartFile[] files) throws IOException {
         String url = discoveryClient.getNextServerFromEureka("client1", false).getHomePageUrl();
-        String response = restTemplate().postForEntity(url + "/api/client1/files/upload", requestEntity, String.class).getBody();
-        System.out.println(response);
+        String serverUrl = url + "/api/client1/files/upload";
+
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = createRequestEntity(files);
+
+        return restTemplate().postForEntity(serverUrl, requestEntity, String.class).getBody();
     }
 
     private HttpEntity<MultiValueMap<String, Object>> createRequestEntity(MultipartFile[] files) throws IOException {
@@ -55,10 +60,34 @@ public class ClientController {
 
         MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
         for (MultipartFile file : files) {
-            body.add(file.getOriginalFilename(), new ByteArrayResource(file.getBytes()));
+            body.add("files", new MultipartInputStreamFileResource(file.getInputStream(), file.getOriginalFilename()));
         }
 
         return new HttpEntity<>(body, headers);
+    }
+
+
+    /*
+        Guys from spring recommend to use that class
+        https://github.com/spring-projects/spring-framework/issues/18147#issuecomment-453431470
+     */
+    private class MultipartInputStreamFileResource extends InputStreamResource {
+
+        private final String filename;
+
+        public MultipartInputStreamFileResource(InputStream inputStream, String filename) {
+            super(inputStream);
+            this.filename = filename;
+        }
+        @Override
+        public String getFilename() {
+            return this.filename;
+        }
+
+        @Override
+        public long contentLength() {
+            return -1; // we do not want to generally read the whole stream into memory ...
+        }
     }
 
 }
