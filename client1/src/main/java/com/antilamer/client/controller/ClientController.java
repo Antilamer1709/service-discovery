10a0494c5334d104155682a84039a22cbe5d5ccb
vslobo <antilamer1709@gmail.com>
2020-05-21 16:27:18 +0300

    Send files, prototype

--- client1/src/main/java/com/antilamer/client/controller/ClientController.java
+++ client1/src/main/java/com/antilamer/client/controller/ClientController.java
@@ -1,9 +1,7 @@
 package com.antilamer.client.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("api/client1")
@@ -12,6 +10,12 @@ public class ClientController {
     @GetMapping("/{userId}")
     public String getClient(@PathVariable long userId) {
         return "client1; userId " + userId;
+    }
+
+
+    @PostMapping("files/upload")
+    public void upload(@RequestParam("files") MultipartFile[] files) {
+        System.out.println(files);
     }
 
 }
--- client2/src/main/java/com/antilamer/client/controller/ClientController.java
+++ client2/src/main/java/com/antilamer/client/controller/ClientController.java
@@ -4,11 +4,19 @@ import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api/client2")
@@ -23,13 +31,34 @@ public class ClientController {
     @Autowired
     private EurekaClient discoveryClient;
 
+
     @GetMapping("/{userId}")
     public String getClient(@PathVariable long userId) {
         String url = discoveryClient.getNextServerFromEureka("client1", false).getHomePageUrl();
         String firstClientResponse = restTemplate().getForEntity(url + "/api/client1/" + userId, String.class).getBody();
 
         return firstClientResponse + " client2; userId " + userId;
+    }
 
+
+    @PostMapping("files/upload")
+    public void upload(@RequestParam("files") MultipartFile[] files) throws IOException {
+        HttpEntity<MultiValueMap<String, Object>> requestEntity = createRequestEntity(files);
+        String url = discoveryClient.getNextServerFromEureka("client1", false).getHomePageUrl();
+        String response = restTemplate().postForEntity(url + "/api/client1/files/upload", requestEntity, String.class).getBody();
+        System.out.println(response);
+    }
+
+    private HttpEntity<MultiValueMap<String, Object>> createRequestEntity(MultipartFile[] files) throws IOException {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+        for (MultipartFile file : files) {
+            body.add(file.getOriginalFilename(), new ByteArrayResource(file.getBytes()));
+        }
+
+        return new HttpEntity<>(body, headers);
     }
 
 }
