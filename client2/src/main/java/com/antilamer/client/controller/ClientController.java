31fccdf8890636de95cf25a565e246be0d095be4
vslobo <antilamer1709@gmail.com>
2020-05-21 12:22:47 +0300

    second client

---
+++ client2/src/main/java/com/antilamer/client/controller/ClientController.java
@@ -0,0 +1,18 @@
+package com.antilamer.client.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/client")
+public class ClientController {
+
+    @GetMapping("/{userId}")
+    public String getClient(@PathVariable long userId) {
+        return "client";
+
+    }
+
+}
