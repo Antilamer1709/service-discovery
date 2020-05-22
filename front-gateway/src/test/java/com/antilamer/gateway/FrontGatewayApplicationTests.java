e6a5a3e58aa902d4670bf0e7e604036cd28b6bb1
vslobo <antilamer1709@gmail.com>
2020-05-22 15:59:47 +0300

    front gateway

---
+++ front-gateway/src/test/java/com/antilamer/gateway/FrontGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antilamer.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
