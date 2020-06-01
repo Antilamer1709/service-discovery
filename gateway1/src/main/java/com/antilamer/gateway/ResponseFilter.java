d64898a268c5f056ef6ef8b7e51f5d99241baaa7
vslobo <antilamer1709@gmail.com>
2020-06-01 11:38:33 +0300

    ResponseFilter single gateway

---
+++ gateway1/src/main/java/com/antilamer/gateway/ResponseFilter.java
@@ -0,0 +1,38 @@
+package com.antilamer.gateway;
+
+import brave.Tracer;
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResponseFilter extends ZuulFilter {
+
+    @Autowired
+    private Tracer tracer;
+
+
+    @Override
+    public String filterType() {
+        return "post";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 1;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        ctx.getResponse().addHeader("tmx-correlation-id", tracer.currentSpan().context().traceIdString());
+        return null;
+    }
+}
